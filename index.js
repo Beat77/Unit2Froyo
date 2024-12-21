6567626e960f8f04b2dfba13d82061fcd5472677
Jamey Jones <jpkey764@gmail.com>
2024-12-21 10:56:57 -0500

    froyo ojects

---
+++ index.js
@@ -0,0 +1,10 @@
+const froyo = {
+name: "frozen yogurt",
+flavors: ["chocolate","vanilla","coffee","strawberry"],
+toppings: ["nuts", "chocolate", "candy"]
+   
+};
+
+console.log (froyo.name);
+
+const userPick = prompt ("Enter your selected flavors");
